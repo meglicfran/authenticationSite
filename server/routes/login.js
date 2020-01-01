e640402331532ca2df3a272a9737b6a6b111079f
meglicfran <meglic.fran@gmail.com>
2020-01-01 16:39:57 +0100

    .

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const User = require("../model/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const Joi = require("@hapi/joi");
-const { loginValidation } = require("../validate");
+const express = require('express');
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Joi = require('@hapi/joi');
+const { loginValidation } = require('../validate');
 
 router = express.Router();
 
 // register validation
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const vali = loginValidation(req.body);
   if (vali.error) {
     res.send(vali.error.details[0].message);
@@ -16,17 +16,17 @@ router.post("/", async (req, res) => {
   }
   const user = await User.findOne({ username: req.body.username });
   if (!user) {
-    res.send("Invalid username or password");
+    res.send('Invalid username or password');
     return;
   }
 
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) {
-    res.send("Invalid username or password");
+    res.send('Invalid username or password');
     return;
   }
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).send("Success");
+  res.header('auth-token', token).send('Success');
 });
 
 //export the router
